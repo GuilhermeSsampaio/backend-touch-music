ba86decabacf78e160e0647ff476dde7c5d26f85
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-10-29 16:13:47 -0400

    backend funcionando na square cloud

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Criação e ativação do ambiente virtual
+python3 -m venv venv
+source venv/bin/activate
+
+# Instalação das dependências
+pip install -r requirements.txt
+
+# Executa o servidor com uvicorn, usando a porta 8000 se $PORT não estiver definida
+uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
